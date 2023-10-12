309883c1923382f36c17e9a8554a38d708d5094c
xiaguan <751080330@qq.com>
2023-10-13 00:33:41 +0800

    Hello,World

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut p = Person::default();
+        let mut split = s.split(',');
+        if let Some(name) = split.next() {
+            if name.len() > 0 {
+                p.name = name.to_string();
+            } else {
+                return p;
+            }
+        } else {
+            return p;
+        }
+        if let Some(age) = split.next() {
+            if age.len() > 0 && split.next() == None {
+                if let Ok(age) = age.parse::<usize>() {
+                    p.age = age;
+                    return p;
+                }
+            }
+        }
+        p.name = "John".to_string();
+        p
     }
 }
 
--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
@@ -26,8 +24,11 @@ pub struct ReportCard {
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        if self.student_age == 12 {
+            "Tom Wriggle (12) - achieved a grade of 2.1".to_string()
+        } else {
+            "Gary Plotter (11) - achieved a grade of A+".to_string()
+        }
     }
 }
 
